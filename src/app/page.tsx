16f09be7058552a1f20cfc8543fe158945ef8fc0
Matias <matias.sanchez.0097@gmail.com>
2023-09-15 13:35:55 -0300

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import { LogoutButton } from "@/components/Logout";
-import { SigninButton } from "@/components/Signin";
 import { authOptions } from "@/pages/api/auth/[...nextauth]";
 import { getServerSession } from "next-auth";
 import Link from "next/link";
@@ -8,14 +7,22 @@ import { redirect } from "next/navigation";
 export default async function Home() {
   const session = await getServerSession(authOptions);
 
+  if (!session) {
+    redirect("/api/auth/signin");
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="flex gap-8">
-        <Link href="/api/auth/signin">Go to login</Link>
-
-        <SigninButton />
+      <div className="flex flex-col items-center gap-4">
         <h1>{session?.user?.name}</h1>
-        <LogoutButton />
+
+        <div className="flex gap-8">
+          <Link href="/api/auth/signin" className="ring-2 ring-green-400 rounded-md p-2">
+            Go to login
+          </Link>
+
+          <LogoutButton />
+        </div>
       </div>
     </main>
   );
--- src/components/Logout.tsx
+++ src/components/Logout.tsx
@@ -4,7 +4,7 @@ import { signOut } from "next-auth/react";
 
 export const LogoutButton = () => {
   return (
-    <button style={{ marginRight: 10 }} onClick={() => signOut()}>
+    <button style={{ marginRight: 10 }} className="ring-2 ring-red-400 rounded-md p-2" onClick={() => signOut()}>
       Sign Out
     </button>
   );
