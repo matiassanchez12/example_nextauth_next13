7310156578fcbf63b1aff45cfc4ee65df527b09a
Matias <matias.sanchez.0097@gmail.com>
2023-09-23 02:18:05 -0300

    auth con mongo

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,13 +1,34 @@
 import NextAuth from "next-auth";
 import { AuthOptions } from "next-auth";
 
+import { MongoDBAdapter } from "@auth/mongodb-adapter";
+import { ObjectId } from "mongodb";
+
 import GoogleProvider from "next-auth/providers/google";
+import clientPromise from "@/lib/db/mongoDBclient";
 
 export const authOptions = {
+  adapter: MongoDBAdapter(clientPromise, {
+    databaseName: "auth",
+    collections: {
+      Users: "users",
+      Accounts: "accounts",
+      Sessions: "sessions"
+    }
+  }),
   providers: [
     GoogleProvider({
       clientId: String(process.env.GOOGLE_CLIENT_ID)!,
-      clientSecret: String(process.env.GOOGLE_CLIENT_SECRET)!
+      clientSecret: String(process.env.GOOGLE_CLIENT_SECRET)!,
+      profile(profile) {
+        return {
+          id: String(ObjectId.generate()),
+          name: profile.name,
+          email: profile.email,
+          image: profile.picture,
+          role: "USER"
+        };
+      }
     })
   ],
   theme: {
