bcbbf508100587157134b6c6dd4e76e8737da28e
Matias <matias.sanchez.0097@gmail.com>
2023-09-23 00:39:19 -0300

    auth with prisma files and configurations

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,9 +1,12 @@
+import { prisma } from "@/lib/db";
+import { PrismaAdapter } from "@auth/prisma-adapter";
 import NextAuth from "next-auth";
 import { AuthOptions } from "next-auth";
 
 import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions = {
+  adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId: String(process.env.GOOGLE_CLIENT_ID)!,
